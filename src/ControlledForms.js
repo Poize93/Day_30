9db638ed9d47f50fedcbc0fd2453fe1edf6e86aa
Poize93 <delhi93rsharma@gmail.com>
2022-03-24 09:38:05 +0530

    1 file modified

--- src/ControlledForms.js
+++ src/ControlledForms.js
@@ -1,39 +1,60 @@
-import React from 'react';
+import React, { useSet } from 'react';
 
+function handleSubmit(a) {
+  console.log(a.length);
+  if (a.length < 1) {
+    var error = 'Required Field';
+  } else {
+    error = '';
+  }
+}
 
-
-function handleSubmit(e) {
+function handleChange(e) {
   e.preventDefault();
-  console.log('Hi');
-
+  //console.log(e.target.name ,e.target.value);
+  var result = e.target.value;
+  handleSubmit(result);
 }
 
 function form() {
   return (
     <>
-      <form onSubmit={(e) => handleSubmit(e)}>
+      <form onSubmit={(e) => handleChange(e)}>
         <h3>User Form using Function</h3>
-        
         <label>First Name</label> &nbsp;
-        <input type="text" name="firstName" class="firstName" ></input>
-        <br />
+        <input
+          type="text"
+          name="firstName"
+          onChange={(e) => handleChange(e)}
+        ></input>
+        <h6>A</h6>
         <br />
         <label>Last Name</label> &nbsp;
-        <input type="text" name="lastName"  />
+        <input type="text" name="lastName" onChange={(e) => handleChange(e)} />
         <br />
         <br />
         <label>Email</label> &nbsp;
-        <input type="text" name="email"  />
+        <input type="text" name="email" onChange={(e) => handleChange(e)} />
         <br />
         <br />
         <label required>Gender</label> &nbsp;
-        <input type="Radio" name="gender" value="male" />
+        <input
+          type="Radio"
+          name="gender"
+          value="male"
+          onChange={(e) => handleChange(e)}
+        />
         Male
-        <input type="Radio" name="gender" value="female" />
+        <input
+          type="Radio"
+          name="gender"
+          value="female"
+          onChange={(e) => handleChange(e)}
+        />
         Female <br />
         <br />
         <label>Courses</label>
-        <select name="courses">
+        <select name="courses" onChange={(e) => handleChange(e)}>
           <option value="React">React</option>
           <option value="node">NodeJS</option>
           <option value="mongo">MongoDB</option>
